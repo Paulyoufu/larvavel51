21f4b14e2b6a8e9fe73cca8ad5c472f30f69b556
Paulyoufu <liangqiangcsharp@qq.com>
2015-11-19 11:54:10 +0800

    创建数据库
    表 字段的修改

--- resources/views/sites/about.blade.php
+++ resources/views/sites/about.blade.php
@@ -1,6 +1,7 @@
 @extends('app')
 @section('content')
     <h3>people i admire</h3>
+    @if(count($people)>0)
     <ul>
         @foreach($people as $person)
             <li>
@@ -8,4 +9,5 @@
             </li>
         @endforeach
     </ul>
+    @endif
 @stop
