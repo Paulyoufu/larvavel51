fd54964e54e47b2d18abb2ccf519de65e2f407f3
Paulyoufu <liangqiangcsharp@qq.com>
2015-11-20 11:25:55 +0800

    链接到文章列表有3种方法
    /articles/{{ $article->id }}
              {{ url('articles',$article->id) }}
        {{ action('ArticlesController@show',[$article->id])

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,9 +3,9 @@
     <h1>articles</h1>
     <hr>
     @foreach($articles as $article)
-
+            <h2><a href="/articles/{{ $article->id }}">{{ $article->title }} one</a></h2>
            <h2><a href="{{ url('articles',$article->id) }}">{{ $article->title }} two</a></h2>
-           
+            <h2><a href="{{ action('ArticlesController@show',[$article->id]) }}">{{ $article->title }} three</a></h2>
             <article>
                 <div class="body">
                    {{ $article->content }}
